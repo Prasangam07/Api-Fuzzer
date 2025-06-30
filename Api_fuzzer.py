82382b26c53edcbd4356898ed24ad3617471c7a4
𝙋𝙧𝙖𝙨𝙖𝙣𝙜𝙖𝙢 - 𝙀𝙩𝙝𝙞𝙘𝙖𝙡 𝙃𝙖𝙘𝙠𝙚𝙧 <147987915+Prasangam07@users.noreply.github.com>
2025-06-30 10:05:05 +0545

    Update Api_fuzzer.py

--- Api_fuzzer.py
+++ Api_fuzzer.py
@@ -1,7 +1,21 @@
+# ================================================
+# 🚨 DISCLAIMER:
+# This script is created for educational purposes 
+# only. Use it responsibly and only on websites 
+# you own or have explicit permission to access.
+#
+# Unauthorized access to computer systems is illegal 
+# and punishable under local and international laws.
+#
+# The author (Prasangam Pathak) is not responsible 
+# for any misuse or damage caused by this script.
+# ================================================
+
 import requests
 
 import sys
-website = input("Enter the website URL: ")
+print(" Enter the url in this format : http//:example.com/api ")
+website = input("Enter the website URL: ") 
 
 res = requests.get(url=website)
 if res.status_code == 200:
@@ -13,3 +27,4 @@ data = res.json()
 
 print("Data fetched from the website:")
 print(data)
+
